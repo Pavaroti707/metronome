aa5fc41683b90dc02839268679690bd0a8753d11
Pavaroti707 <hasicmahir@gmail.com>
2021-05-24 23:52:48 +0200

    metronome

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import  { render } from "react-dom";
+import Metronome from "./Metronome";
+import React from 'react';
+import  "./index.css";
+
+render(
+    <Metronome/>,
+    document.getElementById('root')
+);
+
